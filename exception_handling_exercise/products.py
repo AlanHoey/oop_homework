68c26e79f908c6f0afec6f5ba30fb7fce298e4bd
Alan Hoey <d00243008@student.dkit.ie>
2026-02-26 12:08:20 +0000

    Constructing-B (products.py): Building Product class using validation and exceptions

--- exception_handling_exercise/products.py
+++ exception_handling_exercise/products.py
@@ -9,6 +9,39 @@ class Product:
         a quantity
     '''
 
+    def __init__(self, prod_id: str, prod_name: str, cost_price: float, retail_price: float, units_in_stock: int):
+
+        self.prod_id = prod_id
+        try:
+            if prod_id is None or str(prod_id).strip() == "":
+                raise ValueError("Invalid product ID")
+        except ValueError:
+            print("Invalid product") ID, defaulting to 'unknown_id'")
+            self.prod_id = "unknown_id"
+
+
+        self.prod_name = prod_name
+        if prod_name is None or str(prod_name).strip() == "":
+            print("Invalid product name, defaulting to 'unknown_product'")
+            self.prod_name = "unknown_product"
+
+        self.cost_price = cost_price
+        if cost_price is None or str(cost_price).strip() == "":
+            print("Invalid cost price, defaulting to 0.0")
+            self.cost_price = 0.0
+
+        self.retail_price = retail_price
+        if retail_price is None or str(retail_price).strip() == "":
+            print("Invalid retail price, defaulting to 0.0")
+            self.retail_price = 0.0
+
+        self.units_in_stock = units_in_stock
+        if units_in_stock is None or str(units_in_stock).strip() == "":
+            print("Invalid units in stock, defaulting to 0")
+            self.units_in_stock = 0
+
+
+
 class Book(Product):
     '''
         Code for the Book class goes here
