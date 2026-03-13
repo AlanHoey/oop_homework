20a4639085a29ef733edbbd81bdaa0ebffd0d0cb
Alan Hoey <d00243008@student.dkit.ie>
2026-03-13 11:38:34 +0000

    Feat (main.py): Building "read grades" method v2

--- Wk6_Custom Exceptions/main.py
+++ Wk6_Custom Exceptions/main.py
@@ -5,8 +5,12 @@ grades_file = "grades.txt"
 def read_grades(grades_file: str) -> dict[str, Student]:
     students: dict[str, Student] = {}
 
-
-
-
-
-
+    try:
+        with open(grades_file, "r") as grades_file:
+            for line in grades_file:
+                line = line.strip()
+                components = line.split(",")
+                student_id = components[0]
+                name = components[1]
+                grades_str = components[2].split(",")
+                grades: dict[str, float] = {}
