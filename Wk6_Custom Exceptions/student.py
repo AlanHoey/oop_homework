fa33c3fd7a7f4711dc240580a6d5c59d1318868d
Alan Hoey <d00243008@student.dkit.ie>
2026-03-13 08:53:15 +0000

    Feat (student.py): Building validate_id

--- Wk6_Custom Exceptions/student.py
+++ Wk6_Custom Exceptions/student.py
@@ -8,6 +8,15 @@ class Student:
     '''
 
     def __init__(self, student_id: str, name: str, grades: dict[str, float]):
-        self.student_id = student_id
-        self.name = name
-        self.grades = grades
+        self._student_id = student_id
+        self._name = name
+        self._grades = grades
+
+    def validate_id(self, student_id):
+        if not student_id:
+            raise ValueError('Student ID is required')
+
+        if student_id[:3].upper() != "D00":
+            raise ValueError("Student ID must start with D00")
+
+
