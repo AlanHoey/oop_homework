cdd1133e1aa93e61badbdba1977bc59f51a14dd7
Alan Hoey <d00243008@student.dkit.ie>
2026-03-13 09:11:38 +0000

    Feat (student.py): Building validate_grade

--- Wk6_Custom Exceptions/student.py
+++ Wk6_Custom Exceptions/student.py
@@ -4,13 +4,15 @@ class Student:
     Student has:
     student_id
     name
+    subject_name
     dict[subject name: grade]
     '''
 
-    def __init__(self, student_id: str, name: str, grades: dict[str, float]):
+    def __init__(self, student_id: str, name: str, subject_name: str, grade: dict[str, int]):
         self._student_id = student_id
         self._name = name
-        self._grades = grades
+        self._subject_name = subject_name
+        self.__grade = grade
 
     def validate_id(self, student_id):
         if not student_id:
@@ -18,5 +20,12 @@ class Student:
 
         if student_id[:3].upper() != "D00":
             raise ValueError("Student ID must start with D00")
+
+        return student_id.upper()
+
+    def validate_grade(self, grade):
+
+
+
 
 
