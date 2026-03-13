6b848b688c565237f69d34d9bde5636cfb2f34db
Alan Hoey <d00243008@student.dkit.ie>
2026-03-13 11:02:52 +0000

    Feat (main.py): Building "read grades" method

---
+++ Wk6_Custom Exceptions/main.py
@@ -0,0 +1,12 @@
+from student import Student, IDError, GradeError
+
+grades_file = "grades.txt"
+
+def read_grades(grades_file: str) -> dict[str, Student]:
+    students: dict[str, Student] = {}
+
+
+
+
+
+
